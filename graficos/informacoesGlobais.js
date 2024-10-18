afb43775748e9a22aded742a809b78acc9a8e636
n4ti <108287232+n4ticorrea@users.noreply.github.com>
2024-10-18 11:20:05 -0300

    at

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,10 +6,8 @@ async function vizualizarinformacoesGlobais() {
     cosole.log(dados);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e 
-    que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social
-    e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-        console.log(paragrafo)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
   }
