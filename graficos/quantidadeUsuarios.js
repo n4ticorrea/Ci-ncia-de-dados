fc1dbb22e849bfef9a0b750b0a8f8b5303a51a74
n4ti <108287232+n4ticorrea@users.noreply.github.com>
2024-11-01 11:49:49 -0300

    .

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,23 +1,29 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-  
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeUsuarios,
-      type: 'bar'
-    }
-  ]
-  
-  const grafico = document.createElement('div')
-  grafico.className = 'grafico'
-  document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
-  
+async function objetivosUsuarios() {
+  const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/razoes-globais.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const objetivosUsuarios = Object.keys(dados)
+  const quantidadeUsuarios = Object.values(dados)
+
+const data = [
+  {
+    labels: objetivosUso,
+    values: quantidadeUsuarios,
+    type: 'pie'
   }
-  
-  quantidadeUsuarios()
+]
+
+const layout = 
+  {
+    height: 600,
+    width: 870,
+  }
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data, layout)
+
+}
+
+objetivosUsuarios()
